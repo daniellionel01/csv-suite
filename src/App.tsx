0bf4002769387838c37594b02844ba5770cb450f
Daniel Kurz <danielkurz03@protonmail.com>
2024-06-25 12:49:02 +0200

    add explanatory diagram

--- src/App.tsx
+++ src/App.tsx
@@ -9,31 +9,36 @@ export const App: Component = () => {
 
   return (
     <div class="w-full px-8 py-4">
-      <div class="tabs tabs-lg tabs-bordered">
-        <a
-          classList={{ tab: true, "tab-active": tab() === "split" }}
-          onClick={() => setTab("split")}
-        >
-          SPLIT
-        </a>
-        <a
-          classList={{ tab: true, "tab-active": tab() === "diff" }}
-          onClick={() => setTab("diff")}
-        >
-          DIFF
-        </a>
-        <a
-          classList={{ tab: true, "tab-active": tab() === "filter" }}
-          onClick={() => setTab("filter")}
-        >
-          FILTER
-        </a>
-        <a
-          classList={{ tab: true, "tab-active": tab() === "merge" }}
-          onClick={() => setTab("merge")}
-        >
-          MERGE
-        </a>
+      <div class="w-full flex items-center">
+        <div class="tabs tabs-lg tabs-bordered w-full">
+          <a
+            classList={{ tab: true, "tab-active": tab() === "split" }}
+            onClick={() => setTab("split")}
+          >
+            SPLIT
+          </a>
+          <a
+            classList={{ tab: true, "tab-active": tab() === "diff" }}
+            onClick={() => setTab("diff")}
+          >
+            DIFF
+          </a>
+          <a
+            classList={{ tab: true, "tab-active": tab() === "filter" }}
+            onClick={() => setTab("filter")}
+          >
+            FILTER
+          </a>
+          <a
+            classList={{ tab: true, "tab-active": tab() === "merge" }}
+            onClick={() => setTab("merge")}
+          >
+            MERGE
+          </a>
+        </div>
+        <div class="text-center">
+          <a href="https://link.excalidraw.com/l/2Y6oR19oInE/AG1PNTvCjrF" target="_blank" class="hover:underline">Explanatory Diagram</a>
+        </div>
       </div>
       <div class="pt-10">
         <Show when={tab() === "split"}>
